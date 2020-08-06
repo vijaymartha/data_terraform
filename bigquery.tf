2907d9d5ef770b56353c11f434b95190ebf019c1
Vijay Kumar Martha <marthavijay4@gmail.com>
2020-08-06 16:35:03 +0530

    removed quotes

--- bigquery.tf
+++ bigquery.tf
@@ -1,5 +1,5 @@
 resource "google_project_service" "project" {
-  project = "local.gcp_project"
+  project = local.gcp_project
   service = "iam.googleapis.com"
   disable_dependent_services = true
 }
